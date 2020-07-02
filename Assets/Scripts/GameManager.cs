0db7fe92033a83c966322c1b77e34911369930f3
Eric Kim <erickim@Erics-MacBook-Air.local>
2020-07-02 12:32:58 +0900

    Raining spheres

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnSphere());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnSphere()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float xRange = 15;
+    private float ySpawnPos = 140;
+    private float zRange = 15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos, Random.Range(-zRange, zRange));
     }
 }
