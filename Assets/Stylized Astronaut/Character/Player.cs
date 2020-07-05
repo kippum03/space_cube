3a0d987f7b1016262cb880fd58e6cbd1af8cbcee
Eric Kim <erickim@Erics-MacBook-Air.local>
2020-07-05 12:14:21 +0900

    Fixed sphere collision/impact

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -7,11 +7,17 @@ public class Player : MonoBehaviour
 	private Animator anim;
 	private CharacterController controller;
 
+
+
+
 	public float speed = 600.0f;
 	public float jumpSpeed = 600.0f;
 	public float turnSpeed = 400.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	public float gravity = 20.0f;
+	private Vector3 impact = Vector3.zero;
+	private float mass = 40.0f;
+	
 
 	void Start()
 	{
@@ -36,7 +42,6 @@ public class Player : MonoBehaviour
 
 			if (Input.GetKey(KeyCode.Space))
             {
-				//anim.SetTrigger("Jump_trig"); Delayed, waits for idle to end before commence
 				moveDirection.y += jumpSpeed * Time.deltaTime;
 			}
 		}
@@ -45,5 +50,23 @@ public class Player : MonoBehaviour
 		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 		controller.Move(moveDirection * Time.deltaTime);
 		moveDirection.y -= gravity * Time.deltaTime;
+		if (impact.magnitude > 0.2) controller.Move(impact * Time.deltaTime);
+		impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
 	}
+
+	void AddImpact(Vector3 dir, float force)
+		{
+			dir.Normalize();
+			if (dir.y < 0) dir.y = -dir.y; // reflect down force on the ground
+			impact += dir.normalized * force / mass;
+		}
+	private void OnCollisionEnter(Collision col)
+		{
+			if(col.gameObject.tag == "Sphere")
+			{
+				Debug.Log("impact function");
+				AddImpact(col.gameObject.GetComponent<Rigidbody>().velocity , 3000);
+			}
+
+		}
 }
